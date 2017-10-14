f656bfce7ca0c2e7b86d5a1e8d7957c30327c8b4
nanj <279956327@qq.com>
2017-10-14 22:08:39 +0800

    real time canny with slider ok

--- piCamera.py
+++ piCamera.py
@@ -3,12 +3,30 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+
+
+def upperThresholdsSlider(x):
+	pass
+
+
+def lowerThresholdsSlider(x):
+	pass
+	
+cv2.namedWindow('canny')	
+cv2.createTrackbar('upper', 'canny', 0, 1000, upperThresholdsSlider)
+cv2.createTrackbar('lower', 'canny', 0, 1000, lowerThresholdsSlider)
+
+
+
+
+
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.framerate = 32
-camera.hflip = True
-camera.vflip = True
+camera.framerate = 10
+#camera.hflip = True
+#camera.vflip = True
 rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
 time.sleep(0.1)
@@ -17,12 +35,20 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
+    
+    upper = cv2.getTrackbarPos('upper', 'canny')
+    lower = cv2.getTrackbarPos('lower', 'canny')
+    
     # show the frame
     cv2.imshow("Frame", image)
     
   #  cannyWithSlider(image)
     
-    canny = cv2.Canny(image, 100, 200)
+    
+    
+    
+    
+    canny = cv2.Canny(image, upper, lower)
     
     
     cv2.imshow("canny", canny)
